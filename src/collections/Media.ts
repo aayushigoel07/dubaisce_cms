636ae17e58e1182a3dc9fb44d7a9122922f8b139
aayushigoel07 <agoel7@rockets.utoledo.edu>
2026-03-24 18:10:06 +0400

    fixing media error

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -5,6 +5,30 @@ export const Media: CollectionConfig = {
   access: {
     read: () => true,
   },
+  hooks: {
+    afterRead: [
+      ({ doc }) => {
+        const baseUrl = (process.env.NEXT_PUBLIC_S3_PUBLIC_URL || '').replace(/\/$/, '')
+
+        if (!baseUrl || !doc) return doc
+
+        if (doc.filename) {
+          doc.url = `${baseUrl}/${doc.filename}`
+        }
+
+        if (doc.sizes && typeof doc.sizes === 'object') {
+          Object.keys(doc.sizes).forEach((sizeName) => {
+            const size = doc.sizes?.[sizeName]
+            if (size && typeof size === 'object' && size.filename) {
+              size.url = `${baseUrl}/${size.filename}`
+            }
+          })
+        }
+
+        return doc
+      },
+    ],
+  },
   fields: [
     {
       name: 'alt',
