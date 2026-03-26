d075828b251aab6b93cea5c643b9e7be6bede261
aayushigoel07 <agoel7@rockets.utoledo.edu>
2026-03-26 21:31:29 +0400

    fixed media upload to bucket error

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pub-c974c3a6f8034e64933f676e36099618.r2.dev', 
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   // Your Next.js config here
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
